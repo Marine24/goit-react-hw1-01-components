48b28d69ddf2d26b090d8abf0e3eca5be630aca1
Marina <marfen24@i.ua>
2019-12-15 12:33:06 +0200

    Updates

---
+++ precache-manifest.3efcbd57dc62ec0b9960f5ef4c738deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fc1145709958c4efb68427aa0388ac",
+    "url": "/goit-react-hw1-01-components/index.html"
+  },
+  {
+    "revision": "cd8f3117ef3b98f3b3c1",
+    "url": "/goit-react-hw1-01-components/static/css/main.d43335bf.chunk.css"
+  },
+  {
+    "revision": "42641977c3b6bdfd94e6",
+    "url": "/goit-react-hw1-01-components/static/js/2.e109b485.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw1-01-components/static/js/2.e109b485.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd8f3117ef3b98f3b3c1",
+    "url": "/goit-react-hw1-01-components/static/js/main.f89b089f.chunk.js"
+  },
+  {
+    "revision": "b7a74be24000efb0a611",
+    "url": "/goit-react-hw1-01-components/static/js/runtime-main.2110486b.js"
+  }
+]);
